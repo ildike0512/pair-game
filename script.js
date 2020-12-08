47d8985ad3540e681f272600987c106508b5169c
ildike0512 <hajos.ildiko007@gmail.com>
2020-12-08 12:56:17 +0100

    modify

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cards = document.querySelectorAll('.memory-card');
 const cardsFront = document.querySelectorAll('.front-face');
 const cardsBack = document.querySelectorAll('.back-face');
 
-
+let points = 0;
 
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -24,26 +24,29 @@ function flipCard() {
 
     return;
   } 
-
+ 
     // second click
    /*  hasFlippedCard = false; */
     secondCard = this;
-
+   
     checkForMatch();
- }
-
+     }
+   
  function checkForMatch(){
      let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
      isMatch ? disableCards() : unflipCards();
+    
  }
-
+ 
  function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
-
+    
     resetBoard();
+    
   }
+  
   
   function unflipCards() {
         lockBoard = true;
@@ -51,14 +54,15 @@ function flipCard() {
     setTimeout(() => {
       firstCard.classList.remove('flip');
       secondCard.classList.remove('flip');
-
       resetBoard();
     }, 1500);
+    
   }
 
   function resetBoard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
+    
   }
     
  
@@ -67,11 +71,52 @@ function flipCard() {
           let randomPos = Math.floor(Math.random() * 10);
           card.style.order = randomPos;
         });
+        
       })();
   
   cards.forEach(card => card.addEventListener('click', flipCard));
   
-  
+  const showPoints = (points) => {
+    document.querySelector('.akt-points').textContent = points;
+}
 
+const checkPair = () => {
+    const firstCardIcon = document.querySelector('.memory-card');
+    if (firstCardIcon) {
+        const firstIconClass = firstCardIcon.className.split(' ');
+        const pair = document.querySelectorAll(`.memory-card.flip .${firstIconClass.pop()}`);
+        if (pair.length == 2) {
+            points++;
+            showPoints(points);
+            document.querySelectorAll(`.memory-card.flip`).forEach( 
+                card => card.classList.add('found') 
+            );
+        }
+    }
+}
 
+// Stopper.
+let stopperTime = 0;
+let stopperIsRunning = false;
+setInterval( () => {
+    if (!stopperIsRunning) {
+        return;
+    }
+    
+    stopperTime++;
+    const seconds = padNumbers(stopperTime % 60);
+    const minutes = padNumbers(Math.floor(stopperTime / 60) % 60);
+    const time = `${[minutes, seconds].join(':')}`;
+    const stopperFace = document.querySelector('.time');
+    stopperFace.textContent = time;
+}, 1000);
+
+ document.querySelector('.time__display').addEventListener('click', () => {
+    if (stopperIsRunning) {
+        stopperIsRunning = false;
+        stopperTime = 0;
+    } else {
+        stopperIsRunning = true;
+    }
+    });
 
